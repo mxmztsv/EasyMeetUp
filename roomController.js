4cf106d6e330e37cb04359fc2ac75fe5245085d2
RussianHustla <maximilyn10@mail.ru>
2020-10-02 01:40:10 +0500

    creacting room added

--- roomController.js
+++ roomController.js
@@ -7,40 +7,85 @@ $(document).ready(function () {
             case 'table':
                 $('.main-field').append($('<div class="object table-rectangle">Table</div>').draggable({
                     start: function() {
-                        // const nextEl = $(this).next();
-                        // console.log(nextEl);
-                        // $(this).clone(true, true).prependTo('.object-list__box');
-                        // $(this).css('position', 'absolute');
-                        // $(document.body).append($('.object'));
-                        // $('.object').parent = $(document.body);
-                        // $(document).onmousedown(function(e){
-                        //     var X = e.pageX; // положения по оси X
-                        //     var Y = e.pageY; // положения по оси Y
-                        //     console.log("X: " + X + " Y: " + Y); // вывод результата в консоль
-                        //     $('.object').css('top', Y);
-                        //     $('.object').css('left', X);
-                        // });
 
                     },
                     stop: function() {
-                        console.log('coords:',$(this).position())
+                        console.log('coords:',$('.object').position())
                     },
-                    // // helper: "clone",
                     containment: '.main-field'
                     }
                 ))
                 break;
             case 'seat':
-                $('.main-field').append($('<div class="object seat-circle"><img src="assets/img/circle.svg"></div>').draggable())
+                $('.main-field').append($('<div class="object seat-circle"><img src="assets/img/circle.svg"></div>').draggable({
+                    start: function() {
+
+                    },
+                    stop: function() {
+                        console.log('coords:',$(this).position())
+                    },
+                    containment: '.main-field'
+                }))
                 break;
         }
 
     })
 
-    // $(function() {
-    //
-    //     $('.object').draggable();
-    //
-    // });
+    $('#createRoomButton').on('click', function () {
+        const chests = []
+        $('.seat-circle').each(function(i,elem) {
+            chests.push({
+                'chestId' : i,
+                'coordinates': $(elem).position(),
+                'free': true,
+                'user':{},
+            })
+        });
+
+        const tables = []
+
+        $('.table-rectangle').each(function(i,elem) {
+            tables.push({
+                'coordinates': $(elem).position(),
+            })
+        });
+
+        const data = {
+            'chests': chests,
+            'tables': tables,
+            'id': -1,
+        }
+
+        let newRoomState = JSON.stringify(data);
+        console.log(newRoomState)
+
+        var settings = {
+            "url": "https://easymeetup.herokuapp.com/newroom",
+            "method": "POST",
+            "timeout": 0,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "data": JSON.stringify(data),
+        };
+
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+        });
+
+        // $.ajax ({
+        //     url: 'https://easymeetup.herokuapp.com/newroom',
+        //     type: 'POST',
+        //     headers: {
+        //
+        //     },
+        //     cache: false,
+        //     data: data,
+        //     success: function (response) {
+        //         alert(response)
+        //     }
+        // })
+
+    })
 
 })
