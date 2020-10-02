9d689978ec9162a4f76591a07423e327cae544a9
RussianHustla <maximilyn10@mail.ru>
2020-10-02 05:53:07 +0500

    a lot of functions added

--- mainPage.js
+++ mainPage.js
@@ -6,34 +6,40 @@ $(document).ready(function () {
 
     $('#followRoomButton').on('click', function () {
 
-        const roomId = $('roomIdInput').val()
+        const roomId = Number($('#roomIdInput').val().trim())
+
+        console.log(roomId)
         // sessionStorage.setItem('roomId', roomId);
 
-        const myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
+        if (!isNaN(roomId) && roomId != "") {
+            const myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
 
-        // const raw = JSON.stringify(data);
+            // const raw = JSON.stringify(data);
 
-        const requestOptions = {
-            method: 'GET',
-            headers: myHeaders,
-            redirect: 'follow',
-        };
+            const requestOptions = {
+                method: 'GET',
+                headers: myHeaders,
+                redirect: 'follow',
+            };
 
-        const url = "https://easymeetup.herokuapp.com/room/" + roomId
+            const url = "https://easymeetup.herokuapp.com/room/" + roomId
 
-        fetch(url, requestOptions)
-            .then(response => response.text())
-            .then(result => {
-                console.log(result)
-                if (result === "" || result === null || result === undefined) {
-                    alert("Кажется команты с таким ID не существует : (")
-                } else {
-                    sessionStorage.setItem('roomState', result);
-                    document.location.href = "/room.html";
-                }
-            })
-            .catch(error => console.log('error', error));
+            fetch(url, requestOptions)
+                .then(response => response.text())
+                .then(result => {
+                    console.log(result)
+                    if (result === "" || result === null || result === undefined) {
+                        alert("Кажется команты с таким ID не существует : (")
+                    } else {
+                        sessionStorage.setItem('roomState', result);
+                        // document.location.href = "/room.html";
+                    }
+                })
+                .catch(error => console.log('error', error));
+        }
+
+
 
     })
 
